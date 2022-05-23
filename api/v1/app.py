7849c4015a0819a0792b2710bc000ab1bf0f0680
maba-dev <cherif.ba@hotmail.fr>
2022-05-23 09:47:58 +0000

    debugg my api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,11 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-
+app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
+    """ app teardown"""
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host= getenv('HBNB_API_HOST', '0.0.0.0'), port= getenv('HBNB_API_PORT', '5000'))
-    threaded=True
+    app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from email.policy import strict
 from flask import jsonify
 from api.v1.views import app_views
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
